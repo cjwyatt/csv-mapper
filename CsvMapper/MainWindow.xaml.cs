c258a93c42d9b6e6d0612ba8c94e433878f0dca7
Christopher Wyatt <chris@fourcore-computing.co.uk>
2014-11-07 16:48:02 +0000

    Import initial project files.

---
+++ CsvMapper/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿﻿/*
+    CSV Mapper - A tool for mapping the fields in one CSV file to different fields in another CSV file
+    Copyright (C) 2014 Christopher Wyatt
+    
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+using System.Windows;
+
+namespace CsvMapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
